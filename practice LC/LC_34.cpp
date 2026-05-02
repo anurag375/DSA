73a7349a343a28a7d7f5dde030ba867df6311253
Anurag375 <anurag375.2020@gmail.com>
2026-05-02 23:44:36 +0530

    add LC_34.cpp + update files' position

--- practice LC/LC_34.cpp
+++ practice LC/LC_34.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 
-class Solution {
-public:
-    int countOccurrences(vector<int>& arr, int target) {
-        // Your code goes here
-        int n = arr.size();
+class Solution {    // LC 34: Find First and Last Position of Element in Sorted Array
+
+    public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+    // practice (even easier than the optimal)   t = log n
+        int n = nums.size();
         int floor_idx = -1, ceil_idx = -1;
 
         // find ceil idx (modified Lower Bound), ==> ***U*** ONLY for ceil INDEX (NOT for VALUE)
@@ -14,7 +16,7 @@ public:
         while(low <= high){
             int mid = low + (high-low)/2;
 
-            if(arr[mid] <= target){
+            if(nums[mid] <= target){
                 ceil_idx = mid;
                 low = mid + 1;
             }else{
@@ -23,7 +25,7 @@ public:
         }
 
         // NOT found: 
-        if(ceil_idx == -1 || arr[ceil_idx] != target) return 0; 
+        if(ceil_idx == -1 || nums[ceil_idx] != target) return {-1, -1}; 
         // *U* 1st cond. is for: [for target smaller than every element] & [empty array]  
         
         // find floor idx (Lower Bound), ==> ***U*** ONLY for floor INDEX (NOT for VALUE)
@@ -31,28 +33,26 @@ public:
         while(low <= high){
             int mid = low + (high-low)/2;
 
-            if(arr[mid] >= target){
+            if(nums[mid] >= target){
                 floor_idx = mid;
                 high = mid - 1;
             }else{
                 low = mid + 1;
             }
         }
-        
-cout << floor_idx << " , " << ceil_idx << endl;
 
-        return (ceil_idx - floor_idx + 1);
+        return {floor_idx, ceil_idx};
     }
 };
 
-
 int main(){
-    vector<int> nums = {1,2,4,4,4,4,4,5,6,7,8};
-    int x = 4;
-    
+    vector<int> nums = {1,2,3,4};
+    int target = 0;
+
     Solution obj;
-    int count = obj.countOccurrences(nums, x);
-    cout << count << endl;
-    
+    vector<int> ans = obj.searchRange(nums, target);
+
+    cout << ans[0] << "," << ans[1];
+
     return 0;
 }
