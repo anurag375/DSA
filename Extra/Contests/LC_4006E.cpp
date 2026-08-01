a4647c5749deb421afeef824e014d95e76d78f9c
Anurag375 <anurag375.2020@gmail.com>
2026-08-01 21:59:40 +0530

    added new file .\Extra\Contests\LC_4006E.cpp

---
+++ Extra/Contests/LC_4006E.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>	//#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+    // my-way: (contest)   t = n   s = 1
+    // *U* alternating string is ONLY possible when BOTH (0,1) have: abs(zeros - ones) <= 1
+        int ans = 0;  // total valid counts
+        int zeros = 0;
+        int ones = 0;
+
+        for(char ch:s){
+            if(ch == '0') zeros++;
+            else ones++;
+
+            int diff = abs(zeros - ones);
+            if(diff == 0 || diff == 1) ans++;
+        }
+
+        return ans;
+    }
+};
+
+int main(){
+    Solution sol;
+    string s = "00101";     // output: 3
+    // string s = "010";     // output: 3
+    cout << sol.countValidPrefixes(s) << endl;
+    
+    return 0;
+}
