fe3be2cd874ed5995cafe2e6aa5f675d9c1f595d
Anurag375 <anurag375.2020@gmail.com>
2026-08-14 21:23:22 +0530

    modified the file .\practice LC\Stack and Queues\Monotonic Stack, Queue problems\LC_907.cpp

--- practice LC/Stack and Queues/Monotonic Stack, Queue problems/LC_907.cpp
+++ practice LC/Stack and Queues/Monotonic Stack, Queue problems/LC_907.cpp
@@ -3,22 +3,57 @@ using namespace std;
 
 class Solution {    // 907. Sum of Subarray Minimums
 public:
+// optimal:     t = 2n+2n+n = 5n      s = 5n [similar to its time compl.]
+    vector<int> previousSmallerEqualElement(vector<int>& arr){
+        int n = arr.size();
+        stack<int> st;
+        vector<int> psee(n);
+
+        for(int i=0; i<n; i++){
+            while(!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            if(!st.empty()) psee[i] = st.top();     // *U* stores index, not value
+            else psee[i] = -1;  // *U* put out of range index, So '-1'
+            st.push(i);     // *U* stores index, not value
+        }
+        return psee;
+    }
+
+    vector<int> nextSmallerElement(vector<int>& arr){
+        int n = arr.size();
+        stack<int> st;
+        vector<int> nse(n);
+
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && arr[st.top()] > arr[i])    // ***U*** NO '='
+                st.pop();
+            if(!st.empty()) nse[i] = st.top();    // *U* stores index, not value
+            else nse[i] = n;    // *U* put out of range index, So 'n'
+            st.push(i);     // *U* stores index, not value
+        }
+        return nse;
+    }
+
+
     int sumSubarrayMins(vector<int>& arr) {
-    // brute:   t = n*n     s = 1       ==> gives TLE
         int n = arr.size();
         int sum = 0;
         int mod = int(1e9 + 7);
 
+        vector<int> psee = previousSmallerEqualElement(arr);
+        vector<int> nse = nextSmallerElement(arr);
+
         for(int i=0; i<n; i++){
-            int mini = arr[i];
-            for(int j=i; j<n; j++){
-                mini = min(arr[j], mini);
-                sum = (sum + mini) % mod;
-            }
+            int leftSmaller = i - psee[i];
+            int rightSmaller = nse[i] - i;
+            // mini = min(arr[j], mini);
+            sum = (sum + (rightSmaller * leftSmaller * 1LL * arr[i]) % mod) % mod;
+        
         }
         return sum;
     }
 };
+
 
 int main(){
     Solution sol;
@@ -28,3 +63,18 @@ int main(){
     
     return 0;
 }
+
+
+    // brute:   t = n*n     s = 1       ==> gives TLE
+        // int n = arr.size();
+        // int sum = 0;
+        // int mod = int(1e9 + 7);
+
+        // for(int i=0; i<n; i++){
+        //     int mini = arr[i];
+        //     for(int j=i; j<n; j++){
+        //         mini = min(arr[j], mini);
+        //         sum = (sum + mini) % mod;
+        //     }
+        // }
+        // return sum;
