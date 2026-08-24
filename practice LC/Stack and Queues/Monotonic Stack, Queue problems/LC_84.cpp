2ded683b3dc6add1dd8446f5ef8d91a0671c1c75
Anurag375 <anurag375.2020@gmail.com>
2026-08-24 10:51:39 +0530

    modified the file .\practice LC\Stack and Queues\Monotonic Stack, Queue problems\LC_84.cpp

--- practice LC/Stack and Queues/Monotonic Stack, Queue problems/LC_84.cpp
+++ practice LC/Stack and Queues/Monotonic Stack, Queue problems/LC_84.cpp
@@ -2,35 +2,66 @@
 using namespace std;
 
 class Solution {    // 84. Largest Rectangle in Histogram
+
+// brute (striver-way): [precompute PSE, NSE]       ==>     Accepted..
+// t = 2*2n + n = 5n = n  [for both helper functions + loop in the main funstion]
+// s = 2*2n = 4n = n  [for both helper funstions]
+    vector<int> findPSE(vector<int>& heights){
+        int n = heights.size();
+        stack<int> st;
+        vector<int> pse;
+
+        for(int i=0; i<n; i++){
+            while(!st.empty() && heights[st.top()] >= heights[i])   // **U**
+                st.pop();
+            if(!st.empty()) pse.push_back(st.top());   // *U* store indices
+            else pse.push_back(-1);     // Not found..
+            st.push(i);
+        }
+        return pse;
+    }
+        
+    vector<int> findNSE(vector<int>& heights){
+        int n = heights.size();
+        stack<int> st;
+        vector<int> nse(n);
+
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && heights[st.top()] >= heights[i])   // **U**
+                st.pop();
+            if(!st.empty()) nse[i] = st.top(); // *U* store indices
+            else nse[i] = n;     // Not found..
+            st.push(i);
+        }
+        return nse;
+    }
+
 public:
     int largestRectangleArea(vector<int>& heights) {
-    // striver-way:      ==>     
         int n = heights.size();
         int maxArea = 0;
+
+        vector<int> pse = findPSE(heights); // "previous smaller element"
+        vector<int> nse = findNSE(heights); // "next smaller element"
         
         for(int i=0; i<n; i++){
-            int currHeight = heights[i];
-            int currArea;
-            int j = i;
-            while(j<n && heights[j] >= currHeight){
-                currArea = (j - i + 1) * currHeight;
-                maxArea = max(maxArea, currArea);
-                j++;
-            }
+            int currArea = heights[i] * (nse[i] - pse[i] -1);   // *U*
+            maxArea = max(maxArea, currArea);
         }
-        return maxArea;   
+        return maxArea;  
     }
 };
 
 int main(){
     Solution sol;
     vector<int> heights = {2,1,5,6,2,3};     // output: 10
+    // vector<int> heights = {2,4};     // output: 4
     cout << sol.largestRectangleArea(heights) << endl;
     return 0;
 }
 
 
-    // brute (my-way):      ==>     TLE
+    // brute (my-way):  [find subarrays -> find smallest height -> calc. current area -> shortlist maxArea]    ==>     TLE
         // int n = heights.size();
         // int maxArea = 0;
         
