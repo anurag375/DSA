103ea2075435169b2493b8b043cc5d8f41f724ee
Anurag375 <anurag375.2020@gmail.com>
2026-08-26 18:54:35 +0530

    added new file .\practice LC\Stack and Queues\Monotonic Stack, Queue problems\LC_84.cpp

--- practice LC/Stack and Queues/Monotonic Stack, Queue problems/LC_84.cpp
+++ practice LC/Stack and Queues/Monotonic Stack, Queue problems/LC_84.cpp
@@ -1,53 +1,42 @@
-#include<bits/stdc++.h>	//#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Solution {    // 84. Largest Rectangle in Histogram
-
-// brute (striver-way): [precompute PSE, NSE]       ==>     Accepted..
-// t = 2*2n + n = 5n = n  [for both helper functions + loop in the main funstion]
-// s = 2*2n = 4n = n  [for both helper funstions]
-    vector<int> findPSE(vector<int>& heights){
-        int n = heights.size();
-        stack<int> st;
-        vector<int> pse;
-
-        for(int i=0; i<n; i++){
-            while(!st.empty() && heights[st.top()] >= heights[i])   // **U**
-                st.pop();
-            if(!st.empty()) pse.push_back(st.top());   // *U* store indices
-            else pse.push_back(-1);     // Not found..
-            st.push(i);
-        }
-        return pse;
-    }
-        
-    vector<int> findNSE(vector<int>& heights){
-        int n = heights.size();
-        stack<int> st;
-        vector<int> nse(n);
-
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && heights[st.top()] >= heights[i])   // **U**
-                st.pop();
-            if(!st.empty()) nse[i] = st.top(); // *U* store indices
-            else nse[i] = n;     // Not found..
-            st.push(i);
-        }
-        return nse;
-    }
-
-public:
+    public:
     int largestRectangleArea(vector<int>& heights) {
+
+    // optimal: [push in stack (left to right) -> caculate area while poping (right to left)]
+    // t = n + n = n [for traversal (and pushing to stack) + for poping from stack]
+    // s = n
         int n = heights.size();
+        stack<int> st;  // stores similar to the "find smaller element" stack
         int maxArea = 0;
 
-        vector<int> pse = findPSE(heights); // "previous smaller element"
-        vector<int> nse = findNSE(heights); // "next smaller element"
-        
         for(int i=0; i<n; i++){
-            int currArea = heights[i] * (nse[i] - pse[i] -1);   // *U*
-            maxArea = max(maxArea, currArea);
+            while(!st.empty() && heights[st.top()] >= heights[i]){ // perform..
+                int temp = st.top();    // (index)
+                st.pop();
+
+                int nse = i;   // next smaller element (index)
+                int pse = (st.empty()) ? -1 : st.top();   // previous smaller element (index)
+
+                int area = heights[temp] * (nse - pse -1);
+                maxArea = max(maxArea, area);
+            }
+            st.push(i); 
         }
+
+        // handle leftover stack elements [all are arranged in increasing heights]:
+        while(!st.empty()){
+            int temp = st.top();
+            st.pop();
+            int nse = n;
+            int pse = (st.empty()) ? -1 : st.top();
+
+            int area = heights[temp] * (nse - pse -1);
+            maxArea = max(maxArea, area);
+        }
+
         return maxArea;  
     }
 };
@@ -59,6 +48,43 @@ int main(){
     cout << sol.largestRectangleArea(heights) << endl;
     return 0;
 }
+
+
+// brute (striver-way): [precompute PSE, NSE]       ==>     Accepted..
+// t = 2*2n + n = 5n = n  [for both helper functions + loop in the main funstion]
+// s = 2*2n = 4n = n  [for both helper funstions]
+    // vector<int> findPSE(vector<int>& heights){
+    //     int n = heights.size();
+    //     stack<int> st;
+    //     vector<int> pse;
+
+    //     for(int i=0; i<n; i++){
+    //         while(!st.empty() && heights[st.top()] >= heights[i])   // **U**
+    //             st.pop();
+    //         if(!st.empty()) pse.push_back(st.top());   // *U* store indices
+    //         else pse.push_back(-1);     // Not found..
+    //         st.push(i);
+    //     }
+    //     return pse;
+    // }
+        
+    // vector<int> findNSE(vector<int>& heights){
+    //     int n = heights.size();
+    //     stack<int> st;
+    //     vector<int> nse(n);
+
+    //     for(int i=n-1; i>=0; i--){
+    //         while(!st.empty() && heights[st.top()] >= heights[i])   // **U**
+    //             st.pop();
+    //         if(!st.empty()) nse[i] = st.top(); // *U* store indices
+    //         else nse[i] = n;     // Not found..
+    //         st.push(i);
+    //     }
+    //     return nse;
+    // }
+
+
+// =============
 
 
     // brute (my-way):  [find subarrays -> find smallest height -> calc. current area -> shortlist maxArea]    ==>     TLE
