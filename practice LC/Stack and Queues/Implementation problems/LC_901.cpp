fac9874b08d64cffc2eaafbc0cc28dbcadac093e
Anurag375 <anurag375.2020@gmail.com>
2026-09-01 07:31:33 +0530

    modified file .\practice LC\Stack and Queues\Implementation problems\LC_901.cpp

--- practice LC/Stack and Queues/Implementation problems/LC_901.cpp
+++ practice LC/Stack and Queues/Implementation problems/LC_901.cpp
@@ -1,16 +1,17 @@
-#include<bits/stdc++.h>	//#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 class StockSpanner {    // 901. Online Stock Span
 public:
-// my-way: t = n    s = n
-    stack<pair<int,int>> st;
+// optimal (my-way): [similar to Previous Greater Element]:     ===>>   Prefer this...
+// t = (n + n) [for trversing + stack poping]    s = n
+    stack<pair<int,int>> st;     // {price, days span}
     StockSpanner() {
         
     }
     
     int next(int price) {
-        int ans = 1;    // *U* consider its day first...
+        int ans = 1;    // *U* → today's day is always included...
         while(!st.empty() && st.top().first <= price){
             ans += st.top().second;
             st.pop();
@@ -26,12 +27,6 @@ public:
  * StockSpanner* obj = new StockSpanner();
  * int param_1 = obj->next(price);
  * 
- Input
-["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
-[[], [100], [80], [60], [70], [60], [75], [85]]
-Output
-[null, 1, 1, 1, 2, 1, 4, 6]
-
  */
 
 int main(){
@@ -44,6 +39,53 @@ int main(){
     cout << obj->next(60) << endl;
     cout << obj->next(75) << endl;
     cout << obj->next(85) << endl;
+    // Output:
+    // [1, 1, 1, 2, 1, 4, 6]
 
     return 0;
 }
+
+
+
+// // optimal (striver): t, s [almost SAME logic as my-way optimal; just some code difference] 
+// public:
+//     stack<pair<int,int>> st;    // {price, index}
+//     int idx;
+
+//     StockSpanner() {
+//         idx = -1;   // traces the index/order of the funs. calls (days)
+//     }
+    
+//     int next(int price) {
+//         idx++;
+//         int ans = 1;    // *U* current day is always included...
+//         while(!st.empty() && st.top().first <= price)
+//             st.pop();
+
+//         ans = idx - ((st.empty()) ? -1 : st.top().second);    // **U** calc. the index/days diff.
+
+//         st.push({price, idx});
+//         return ans;
+//     }
+
+
+// =====================
+
+
+// // brute:    ==>    NOT imp.
+// // t = (total no. of days)
+//  // s = (total number of "next" func. calls)
+// public:
+//     vector<int> vec;
+//     StockSpanner() {}
+    
+//     int next(int price) {
+//         int ans = 1;    // *U* consider current day first...
+//         for(int i=vec.size()-1; i>=0; i--){
+//             if(vec[i] > price) break;
+//             ans++;
+//         }
+
+//         vec.push_back(price);
+//         return ans;
+//     }
