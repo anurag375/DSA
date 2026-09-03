92f43bc9bdf46f4324ce2c7755613d2ba05cd5cf
Anurag375 <anurag375.2020@gmail.com>
2026-09-03 19:17:33 +0530

    added new file .\6 Stacks and Queues\Prefix, Infix & Postfix conversion problems\Prefix_to_Infix.cpp

---
+++ 6 Stacks and Queues/Prefix, Infix & Postfix conversion problems/Prefix_to_Infix.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string PrefixToInfix(string s) {
+    // t = n + 1 + (n1 + n2)    [for traversal + stack top/pop operations + for adding 2 strings (lang. specific)]        s = n
+        int n = s.size();
+        stack<string> st;
+        
+        unordered_map<char,int> priority = {{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}, {'^', 3}};  // "(" and ")" will be set 0 by default
+        
+        int i = n-1;
+        while(i >= 0){
+            if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))
+                st.push(string(1, s[i]));   // ⭐ char to string conversion..
+            else{
+                string sFirst = st.top();
+                st.pop();
+                string sSecond = st.top();
+                st.pop();
+
+                string newS = sFirst + s[i] + sSecond;
+                st.push("(" + newS + ")");
+            }
+            i--;
+        }
+
+        return st.top();
+    }
+};
+
+int main(){
+    Solution sol;
+    string s = "*+pq-mn";     // Output: ((p+q)*(m-n))
+    cout << sol.PrefixToInfix(s) << endl;
+    return 0;
+}
