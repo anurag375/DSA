c75bbc9c1b11847d2b27d16ca3bd4cc4967e79b8
Anurag375 <anurag375.2020@gmail.com>
2026-09-03 22:17:08 +0530

    added new file .\6 Stacks and Queues\Prefix, Infix & Postfix conversion problems\Prefix_to_Postfix.cpp

--- 6 Stacks and Queues/Prefix, Infix & Postfix conversion problems/Prefix_to_Postfix.cpp
+++ 6 Stacks and Queues/Prefix, Infix & Postfix conversion problems/Prefix_to_Postfix.cpp
@@ -3,27 +3,27 @@ using namespace std;
 
 class Solution {
 public:
-    string PostfixToPrefix(string s) {
+    string PrefixToPostfix(string s) {
     // t = n + 1 + (n1 + n2)    [for traversal + stack top/pop operations + for adding 2 strings (lang. specific)]        s = n
         int n = s.size();
         stack<string> st;
         
         unordered_map<char,int> priority = {{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}, {'^', 3}};  // "(" and ")" will be set 0 by default
         
-        int i = 0;
-        while(i < n){
-            if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))    // 1> operands...
+        int i = n-1;
+        while(i >= 0){
+            if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))    // 1> operands
                 st.push(string(1, s[i]));   // ⭐ char to string conversion..
-            else{   // 2> only operators... [NO parenthesis]
+            else{   // 2> only operators [NO parenthesis]
                 string t1 = st.top();
                 st.pop();
                 string t2 = st.top();
                 st.pop();
 
-                string newS = s[i] + t2 + t1;
+                string newS = t1 + t2 + s[i];
                 st.push(newS);
             }
-            i++;
+            i--;
         }
 
         return st.top();
@@ -32,7 +32,7 @@ public:
 
 int main(){
     Solution sol;
-    string s = "ab-de+f*/";     // Output: /-ab*+def
-    cout << sol.PostfixToPrefix(s) << endl;
+    string s = "/-ab*+def";     // Output: ab-de+f*/
+    cout << sol.PrefixToPostfix(s) << endl;
     return 0;
 }
